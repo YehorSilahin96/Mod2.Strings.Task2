247e6f54fbb864180d6d7dff96620b1b8bc3a11c
YSilahin <egor.silagin96@gmail.com>
2021-06-10 17:52:30 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Do you want to know actual date and time ?" + " Enter Yes/No");
+        String ans = sc.nextLine();
+
+        if (ans.equals("Yes")) {
+
+            Calendar res = Calendar.getInstance();
+
+            System.out.println("Current Date and Time :");
+            System.out.format("%tB %te, %tY%n", res, res, res);
+            System.out.format("%tl:%tM %tp%n", res, res, res);
+        }
+
+        if (ans.equals("No")) {
+
+            System.out.println("OMG, you're so bored !");
+
+        } else
+
+            System.out.println("You're wrong, try again");
+
+    }
+}
